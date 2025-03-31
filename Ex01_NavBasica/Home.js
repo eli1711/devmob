828a6d630b92f8e918f39dda8ef40da65d08dda6
eli1711 <elihofnimarian@gmail.com>
2025-03-31 19:46:39 -0300

    exercicio01_NavBasica

---
+++ Ex01_NavBasica/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, Button, StatusBar } from "react-native";
+import styles from "./styles";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+type Props = NativeStackScreenProps<RootStackParamList>;
+
+export default function Home({ navigation }: Props) {
+  return (
+    <View style={styles.container}> 
+      <StatusBar barStyle="dark-content" />
+      <Text>Home Screen</Text>
+      <Button
+        title="Settings"
+        onPress={() => navigation.navigate("Settings")}
+      />
+    </View>
+  );
+}
