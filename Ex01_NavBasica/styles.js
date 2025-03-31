f986b5f4d95390d37a51fd177d6646aa75e92bf0
eli1711 <elihofnimarian@gmail.com>
2025-03-31 19:55:02 -0300

    atualizado

---
+++ Ex01_NavBasica/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+// Define os estilos para os componentes.
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
